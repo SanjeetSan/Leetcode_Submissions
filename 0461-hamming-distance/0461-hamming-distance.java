23884062acf50f42ccaba29220f1e9a28e1fec8f
Sanjeet <sanjeet.stanly@gmail.com>
2026-08-28 22:29:51 +0530

    Time: 0 ms (100%), Space: 42.4 MB (22.84%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int res = x ^ y;
+        int c = 0;
+        while(res != 0){
+            res = res & (res- 1);
+            c++;
+        }
+        return c;
+    }
+}
