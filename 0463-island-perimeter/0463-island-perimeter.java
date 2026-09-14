3931ed5f9195d3bbbcad964d727c16090327e74a
Sanjeet <sanjeet.stanly@gmail.com>
2026-09-14 22:11:38 +0530

    Time: 9 ms (9.3%), Space: 47.7 MB (7.01%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    q.offer(new int[] { i, j });
+                    grid[i][j] = 2;
+                    break;
+                }
+            }
+            if (!q.isEmpty())
+                break;
+        }
+
+        int[][] dir = {
+                { 0, 1 }, { 1, 0 },
+                { -1, 0 }, { 0, -1 }
+        };
+
+        int answer = 0;
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+
+            for (int sides = 0; sides < 4; sides++) {
+                int rr = curr[0] + dir[sides][0];
+                int rc = curr[1] + dir[sides][1];
+
+                // Outside
+                if (rr < 0 || rc < 0 || rr >= m || rc >= n || grid[rr][rc] == 0) {
+                    answer++;
+                }
+                // Water
+                // else if (grid[rr][rc] == 0) {
+                //     answer++;
+                // }
+                // Unvisited land
+                else if (grid[rr][rc] == 1) {
+                    grid[rr][rc] = 2;
+                    q.offer(new int[] { rr, rc });
+                }
+            }
+        }
+
+        return answer;
+    }
+}
